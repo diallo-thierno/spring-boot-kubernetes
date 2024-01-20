bc6d2407b2fb065be17db164d68948af261a84e5
thierno.diallo <thierno.diallo@ngitech.fr>
2024-01-20 16:10:30 +0100

    http port

--- src/main/java/io/cerdes/springbootkubernetes/SpringBootKubernetesApplication.java
+++ src/main/java/io/cerdes/springbootkubernetes/SpringBootKubernetesApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@KubernetesApplication(ingress = @Ingress(expose = true, host = "spring-boot-kubernetes.io"), serviceType = ServiceType.NodePort, replicas = 2, ports = @Port(name = "web", containerPort = 8080, protocol = Protocol.HTTP))
+@KubernetesApplication(ingress = @Ingress(expose = true, host = "spring-boot-kubernetes.io"), serviceType = ServiceType.NodePort, replicas = 2, ports = @Port(name = "http", containerPort = 8080, protocol = Protocol.HTTP))
 @JibBuild(from = "openjdk:21-jdk-slim", group = "docker.io/tnaddhal", dockerBuild = false)
 public class SpringBootKubernetesApplication {
     public static void main(String[] args) {
