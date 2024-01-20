59a88c7dc08026a6ef7a002e1841b2db7b70263f
thierno.diallo <thierno.diallo@ngitech.fr>
2024-01-20 03:52:26 +0100

    Changing serviceType

--- src/main/java/io/cerdes/springbootkubernetes/SpringBootKubernetesApplication.java
+++ src/main/java/io/cerdes/springbootkubernetes/SpringBootKubernetesApplication.java
@@ -3,11 +3,12 @@ package io.cerdes.springbootkubernetes;
 import io.dekorate.jib.annotation.JibBuild;
 import io.dekorate.kubernetes.annotation.Ingress;
 import io.dekorate.kubernetes.annotation.KubernetesApplication;
+import io.dekorate.kubernetes.annotation.ServiceType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@KubernetesApplication(ingress = @Ingress(expose = true))
+@KubernetesApplication(ingress = @Ingress(expose = true), serviceType = ServiceType.NodePort)
 @JibBuild(from = "openjdk:21-jdk-slim", group = "docker.io/tnaddhal", dockerBuild = false)
 public class SpringBootKubernetesApplication {
     public static void main(String[] args) {
