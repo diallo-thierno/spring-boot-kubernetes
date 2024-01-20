0d9dd0ddd723deee740f838ef8d624520e7fcda0
thierno.diallo <thierno.diallo@ngitech.fr>
2024-01-20 16:05:24 +0100

    replicas set to 2

--- src/main/java/io/cerdes/springbootkubernetes/SpringBootKubernetesApplication.java
+++ src/main/java/io/cerdes/springbootkubernetes/SpringBootKubernetesApplication.java
@@ -1,14 +1,12 @@
 package io.cerdes.springbootkubernetes;
 
 import io.dekorate.jib.annotation.JibBuild;
-import io.dekorate.kubernetes.annotation.Ingress;
-import io.dekorate.kubernetes.annotation.KubernetesApplication;
-import io.dekorate.kubernetes.annotation.ServiceType;
+import io.dekorate.kubernetes.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@KubernetesApplication(ingress = @Ingress(expose = true, host = "spring-boot-kubernetes.io"), serviceType = ServiceType.NodePort, replicas = 10)
+@KubernetesApplication(ingress = @Ingress(expose = true, host = "spring-boot-kubernetes.io"), serviceType = ServiceType.NodePort, replicas = 2, ports = @Port(name = "web", containerPort = 8080, protocol = Protocol.HTTP))
 @JibBuild(from = "openjdk:21-jdk-slim", group = "docker.io/tnaddhal", dockerBuild = false)
 public class SpringBootKubernetesApplication {
     public static void main(String[] args) {
